3488c8247ad4d96bdf05d8ddcfc1722273f3a948
EC2 Default User <ec2-user@ip-172-31-20-117.ap-southeast-1.compute.internal>
2018-12-01 05:41:24 +0000

    a

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,11 +3,11 @@ CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'
 config.fog_credentials = {
  provider: 'AWS',
- aws_access_key_id: ENV['AKIAIG5DUU4ZPQQQWNHQ'],
- aws_secret_access_key: ENV['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'],
- region: 'ap-northeast-1'
+ aws_access_key_id: ENV['ACCESS_KEY_ID'],
+ aws_secret_access_key: ENV['SECRET_ACCESS_KEY'],
+ region: ENV['S3_REGION']
  }
-config.fog_directory = ENV['tsukutta']
+config.fog_directory = ENV['S3_BUCKET']
  config.asset_host = 'https://s3.ap-northeast-1.amazonaws.com/tsukutta'
 end
 end
