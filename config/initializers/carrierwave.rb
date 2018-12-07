c44e0a09638f174b2a660b290534dfe7c51810a6
EC2 Default User <ec2-user@ip-172-31-20-117.ap-southeast-1.compute.internal>
2018-12-07 13:54:26 +0000

    a

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,22 +1,17 @@
-require 'carrierwave/storage/abstract'
-require 'carrierwave/storage/file'
-require 'carrierwave/storage/fog'
-
 
 CarrierWave.configure do |config|
-if Rails.env.production?
-  config.storage :fog
-  config.fog_provider = 'fog/aws'
-  config.fog_directory = 'tsukutta'
- config.fog_credentials = {
- provider: 'AWS',
- aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
- aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
- region: ENV['AWS_REGION'],
- path_style: true
- }
-else
-    config.storage :file
-    config.enable_processing = false if Rails.env.test?
+  config.fog_credentials = {
+    provider: 'AWS',
+    aws_access_key_id: 'AWS_ACCESS_KEY_ID',
+    aws_secret_access_key: 'AWS_SECRET_ACCESS_KEY',
+    region: 'ap-northeast-1'
+  }
+  
+  case Rails.env
+  when 'production'
+        
+  config.fog_directory  = 'tsukuttapic'
+  config.cache_storage = :fog
+  end
 end
-end
+
