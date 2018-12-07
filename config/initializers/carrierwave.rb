f88f8ac22a38f581ccd28a4a37bb16fc583ddbf5
EC2 Default User <ec2-user@ip-172-31-20-117.ap-southeast-1.compute.internal>
2018-12-07 14:06:30 +0000

    a

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,8 +2,8 @@ if Rails.env == 'production'
 CarrierWave.configure do |config|
   config.fog_credentials = {
     provider: 'AWS',
-    aws_access_key_id: 'AWS_ACCESS_KEY_ID',
-    aws_secret_access_key: 'AWS_SECRET_ACCESS_KEY',
+    aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
     region: 'ap-northeast-1'
   }
   
