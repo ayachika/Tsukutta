c247d778329f642ecddcfad35f5e8c347e49fb06
EC2 Default User <ec2-user@ip-172-31-20-117.ap-southeast-1.compute.internal>
2018-12-12 22:28:11 +0000

    a

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,26 +3,24 @@ require 'carrierwave/storage/abstract'
 require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
-
+unless Rails.env.test?
 CarrierWave.configure do |config|
-  if Rails.env.production?
+  config.cache_dir = "#{Rails.root}/tmp/uploads"
   config.fog_provider = 'fog/aws'
   config.fog_credentials = {
     
     provider: 'AWS',
     aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
     aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-    region: ENV['S3_REGION']
   }
   
-  config.cache_storage = :fog    
   config.fog_directory  = ENV['S3_BUCKET']
+  config.cache_storage = :fog    
   config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/tsukuttapic'
-  else
-  config.storage :file
-  config.enable_processing = false if Rails.env.test?
-  
-  
   end
-end
+else
+    CarrierWave.configure do |config|
+    config.storage = :file
+    end
+  end
 
